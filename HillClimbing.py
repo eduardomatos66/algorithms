0522b2f212486d59ae2c5125588e548101b25d99
eduardomatos66 <2d5c80546cfaf7abab1d9864c20119ea07e8e2cc>
2018-05-20 10:09:18 -0300

    Updating Shuffle to do random trials

--- HillClimbing.py
+++ HillClimbing.py
@@ -23,7 +23,7 @@ E3 (C1, C2, C4, C3, C5, C6, C7, C8, C9, C10, C11, C12) etc ...
 
 """
 import random
-
+import sys
 
 COMPUTER_DISTANCES = {
     'C1,C2': 30, 'C1,C3': 84, 'C1,C4': 56, 'C1,C6': 70, 'C1,C8': 75, 'C1,C10': 40, 'C1,C12': 10, 'C2,C3': 65,
@@ -33,6 +33,7 @@ COMPUTER_DISTANCES = {
     'C8,C11': 45, 'C9,C10': 23, 'C9,C12': 68, 'C10,C11': 10, 'C11,C12': 14
 }
 
+
 class Entry(object):
     """
     Default entry with representation (positions sequence) and its value.
@@ -40,13 +41,34 @@ class Entry(object):
 
     def __init__(self, positions_sequence):
         self.positions_sequence = positions_sequence
-        self.value = -99999999999999
+        self.value = sys.maxsize
 
     def __eq__(self, other):
         return other.positions_sequence == self.positions_sequence
 
     def __str__(self):
         return str(self.positions_sequence)
+
+    def evaluate_function(self):
+        """
+        Evaluate entry. In the problem, it is the distance between sequence of Computers
+        :param entry: Entry
+        :return: value of entry
+        """
+        value = 0
+        for pos in range(len(self.positions_sequence)-1):
+            a = self.positions_sequence[pos]
+            b = self.positions_sequence[pos+1]
+            if a and b and '{},{}'.format(a, b) in COMPUTER_DISTANCES:
+                value += COMPUTER_DISTANCES.get('{},{}'.format(a, b))
+            elif a and b and '{},{}'.format(b, a) in COMPUTER_DISTANCES:
+                value += COMPUTER_DISTANCES.get('{},{}'.format(b, a))
+            else:
+                print('It was not possible to find possible connection between {} and {} in {}'.format(a, b, self))
+                value = -1
+                return
+        print('{} has value {}'.format(self, value))
+        self.value = value
 
 
 def get_random_init(position_set):
@@ -55,28 +77,14 @@ def get_random_init(position_set):
     :param position_set: list of positions
     :return: random position of set
     """
-    return position_set[random.randrange(0, len(position_set)-1)]
-
-
-def evaluate_function(entry):
-    """
-    Evaluate entry. In the problem, it is the distance between sequence of Computers
-    :param entry: Entry
-    :return: value of entry
-    """
-    entry.value = 0
-    for pos in range(len(entry.positions_sequence)-1):
-        a = entry.positions_sequence[pos]
-        b = entry.positions_sequence[pos+1]
-        if a and b and '{},{}'.format(a, b) in COMPUTER_DISTANCES:
-            entry.value += COMPUTER_DISTANCES.get('{},{}'.format(a, b))
-        elif a and b and '{},{}'.format(b, a) in COMPUTER_DISTANCES:
-            entry.value += COMPUTER_DISTANCES.get('{},{}'.format(b, a))
-        else:
-            print('It was not possible to find possible connection between {} and {} in {}'.format(a, b, entry))
-            entry.value = -99999999999999
-            return
-    print('{} has value {}'.format(entry, entry.value))
+    random.shuffle(position_set)
+    entry = Entry(position_set)
+    entry.evaluate_function()
+    while entry.value < 0:
+        random.shuffle(position_set)
+        entry = Entry(position_set)
+        entry.evaluate_function()
+    return entry
 
 
 def permute_positions(entry):
@@ -105,7 +113,7 @@ def get_best_neighbor(position):
     neighboors = permute_positions(position)
     best_neighbor = None
     for entry in neighboors:
-        evaluate_function(entry)
+        entry.evaluate_function()
         best_neighbor = best_neighbor if (best_neighbor and best_neighbor.value <= entry.value) else entry
 
     return best_neighbor
@@ -128,7 +136,7 @@ def hill_climbing(init, valid_set):
     """
     climbing = True
     current = init or get_random_init(valid_set)
-    evaluate_function(current)
+    current.evaluate_function()
     while True:
         best_neighbor = get_best_neighbor(current)
         if (best_neighbor and current) and best_neighbor.value >= current.value:
@@ -136,6 +144,9 @@ def hill_climbing(init, valid_set):
         elif not best_neighbor:
             return current
         current = best_neighbor
+        print('======================================== {} - {} ======================================== '.format(
+            current, current.value)
+        )
 
 
 def main():
@@ -143,9 +154,16 @@ def main():
     Main method to algorithm
     :return:
     """
-    initial_entry = Entry(['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12'])
+    initial_position = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12']
+
+    # initial_position = ['C8', 'C5', 'C4', 'C1', 'C12', 'C11', 'C3', 'C6', 'C9', 'C7', 'C10', 'C2']
+    initial_entry = Entry(initial_position)
+    initial_entry.evaluate_function()
+
+    # max = hill_climbing(None, initial_position)
     max = hill_climbing(initial_entry, None)
-    print('\nThe best option is {}'.format(max))
+
+    print('\nThe best option is {} with value equals to {}'.format(max, max.value))
 
 
 if __name__ == '__main__':
